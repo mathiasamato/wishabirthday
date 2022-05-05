032394797e5a37687b09c0584a6cb1da60ba968e
mathiasamato <mathias.amt@eduge.ch>
2022-05-05 11:30:07 +0200

    Ajout du LanguageId dans les messages

--- src/model/messageRandomModel.php
+++ src/model/messageRandomModel.php
@@ -2,8 +2,8 @@
 try {
 
     static $ps = null;
-    $sql = 'INSERT INTO `Messages` (`CreatedBy`, `CreatedFor`, `Text`, `IsPrivate`)';
-    $sql .= 'VALUES(:CREATEDBY, :CREATEDFOR, :TEXT, :ISPRIVATE);';
+    $sql = 'INSERT INTO `Messages` (`CreatedBy`, `CreatedFor`, `Text`, `IsPrivate`, `LanguageId`)';
+    $sql .= 'VALUES(:CREATEDBY, :CREATEDFOR, :TEXT, :ISPRIVATE, :LANGUAGEID);';
 
     if ($ps == null) { //if the ps variable is null, it means that the prepare statement has not been set yet
         $ps = dbConnect()->prepare($sql); //prepare the sql query
@@ -14,8 +14,6 @@ try {
 
     $connectedUser = GetUserById($_SESSION['connectedUserId']); //Get the user with the corresponding id
 
-    $connectedUserId = $connectedUser['Id']; //Get the id of that selected user
-
     $randomUser = GetRandomUser(); //Get one random user
 
     while($connectedUser['Id'] == $randomUser['Id']){  //Make sure that the sender is not the same as the receiver
@@ -24,18 +22,17 @@ try {
 
     }
 
-    $randomUserId = $randomUser['Id']; //Get the id of that random user
-
     $isPrivate = 0;
 
     if(isset($_POST['IsPrivate'])){
         $isPrivate = 1;
     }
 
-    $ps->bindParam(':CREATEDBY', $connectedUserId, PDO::PARAM_STR); //Bind all the parameters
-    $ps->bindParam(':CREATEDFOR', $randomUserId, PDO::PARAM_STR);
+    $ps->bindParam(':CREATEDBY', $connectedUser['Id'], PDO::PARAM_STR); //Bind all the parameters
+    $ps->bindParam(':CREATEDFOR', $randomUser['Id'], PDO::PARAM_STR);
     $ps->bindParam(':TEXT', $_POST['TextToSendToRandomUser'], PDO::PARAM_STR);
     $ps->bindParam(':ISPRIVATE', $isPrivate);
+    $ps->bindParam(':LANGUAGEID', $connectedUser['LanguageId']);
 
     if ($ps->execute()){ //Execute the prepare statement and send a validation email
 
--- src/model/messageTargetModel.php
+++ src/model/messageTargetModel.php
@@ -2,8 +2,8 @@
 try {
 
     static $ps = null;
-    $sql = 'INSERT INTO `Messages` (`CreatedBy`, `CreatedFor`, `Text`, `IsPrivate`)';
-    $sql .= 'VALUES(:CREATEDBY, :CREATEDFOR, :TEXT, :ISPRIVATE);';
+    $sql = 'INSERT INTO `Messages` (`CreatedBy`, `CreatedFor`, `Text`, `IsPrivate`, `LanguageId`)';
+    $sql .= 'VALUES(:CREATEDBY, :CREATEDFOR, :TEXT, :ISPRIVATE, :LANGUAGEID);';
 
     if ($ps == null) { //if the ps variable is null, it means that the prepare statement has not been set yet
         $ps = dbConnect()->prepare($sql); //prepare the sql query
@@ -22,10 +22,11 @@ try {
         $isPrivate = 1;
     }
 
-    $ps->bindParam(':CREATEDBY', $connectedUserId, PDO::PARAM_STR); //Bind all the parameters
+    $ps->bindParam(':CREATEDBY', $connectedUser['Id'], PDO::PARAM_STR); //Bind all the parameters
     $ps->bindParam(':CREATEDFOR', $_SESSION["userInfos"]['Id'], PDO::PARAM_STR);
     $ps->bindParam(':TEXT', $_POST['TextToSendToTargetUser'], PDO::PARAM_STR);
     $ps->bindParam(':ISPRIVATE', $isPrivate);
+    $ps->bindParam(':LANGUAGEID', $connectedUser['LanguageId']);
 
     if ($ps->execute()){ //Execute the prepare statement and send a validation email
 
